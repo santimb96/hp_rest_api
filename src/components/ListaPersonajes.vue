d0e0cd91030707295e67de0495c477eed4a3e424
Santiago <Martnez Bota>
2021-12-20 14:07:50 +0100

    init commit

---
+++ src/components/ListaPersonajes.vue
@@ -0,0 +1,43 @@
+<template>
+<section>
+    <div class="personajes">
+        <div class="personaje_item" v-for="personaje in personajes" :key = "personaje.id">
+            <img :src="personaje.imagen" :alt="personaje.nombre">
+            <h1>{{ personaje.personaje }}</h1>
+            <h2 v-if="personaje.casaDeHogwarts != 'ninguna' ">{{ personaje.casaDeHogwarts }}</h2>
+
+            <h2>{{ personaje.interpretado_por}}</h2>
+             
+        </div>
+    </div>
+</section>
+  
+</template>
+
+<script>
+import { onMounted, computed } from '@vue/runtime-core';
+import { useStore } from 'vuex';
+
+export default {
+    
+    setup(){
+        const store = useStore();
+        const personajes = computed (() => {
+            return store.state.filtroPersonajes;
+        })
+
+        onMounted(() => {
+            store.dispatch('obtenerPersonajes');
+        });
+
+        return {
+            personajes
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,35 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    personajes: [],
+    filtroPersonajes: []
+  },
+  mutations: {
+
+    setPersonajes(state, payload){
+        state.personajes = payload;
+    },
+
+    setFiltroPersonajes(state, payload){
+      state.filtroPersonajes = payload;
+    }
+  },
+  actions: {
+
+      async obtenerPersonajes({commit}){
+
+        try{
+          const respuesta = await fetch('https://fedeperin-harry-potter-api.herokuapp.com/personajes');
+          const datos = await respuesta.json();
+
+          commit('setPersonajes', datos);
+          commit('setFiltroPersonajes', datos);
+          
+        } catch (error){
+          console.error('Error!');
+        }
+      }
+  },
   modules: {},
 });
